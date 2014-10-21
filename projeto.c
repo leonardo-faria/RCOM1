85460641f575d546d074e8dd265349255a7ddf5a
root <root@tux54.netlab.fe.up.pt>
2014-10-21 12:00:36 +0100

    4th commit

--- projeto.c
+++ projeto.c
@@ -1,30 +1,9 @@
 /*Non-Canonical Input Processing*/
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <termios.h>
-#include <stdio.h>
-
-#define BAUDRATE B38400
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
-#define F 0x7E
-#define A 0x03
-#define C 0x07
-#define TRANSMITTER 0
-#define RECEIVER 1
-
-volatile int STOP=FALSE;
+#include "AppLayer.h"
 
 
-int llopen(AppLayer apl);
 
-struct applicationLayer { 
-	int fileDescriptor; /*Descritor correspondente à porta série*/ 
-	int status; /*TRANSMITTER | RECEIVER*/ 
-}typedef AppLayer; 
+
 
 int main(int argc, char** argv)
 {
@@ -83,140 +62,7 @@ apl.fileDescriptor = fd;
 }
 
 
-int llopen(AppLayer apl)
-{
-	int fd=apl.fileDescriptor;
-	char UA[5],SET[5];
-	UA[0] = F;
-	UA[1] = A;
-	UA[2] = C;
-	UA[3] = UA[1]^UA[2];
-	UA[4] = F;
 
-	SET[0] = F;
-	SET[1] = A;
-	SET[2] = 0x03;
-	SET[3] = UA[1]^SET[2];
-	SET[4] = F;
-
-	if(apl.status!=TRANSMITTER)
-	{
-		unsigned char *aux = SET;
-		
-		int size = sizeof(SET) / sizeof(unsigned char);
-		int num = 0;
-		int missing = size;
-
-		while(missing > 0) {
-			num = write(fd,aux,missing);
-			aux += num;
-			missing -= num;
-		}
-
-		unsigned char *temp;
-		int state=0;
-		while(state!=5)
-		{
-			switch(state)
-			{
-				case 0:
-				read(fd,temp, 1);
-				if(*temp==UA[0])
-					state=1;
-				break;
-				case 1:
-				read(fd,temp, 1);
-				if(*temp==UA[1])
-					state=2;
-				else if(*temp!=UA[0])
-					state=0;
-				break;	
-				case 2:
-				read(fd,temp, 1);
-				if(*temp==UA[0])
-					state=1;
-				else if(*temp==UA[2])
-					state=3;
-				else state=0;
-				break;	
-				case 3:
-				read(fd,temp, 1);
-				if(*temp==UA[0])
-					state=1;
-				else if(*temp==UA[3])
-					state=4;
-				else state=0;
-				break;	
-				case 4:
-				
-				read(fd,temp, 1);
-				if(*temp==UA[4])
-					state=5;
-				else state=0;
-				break;	
-			}
-		}
-	}
-	else
-	{
-		unsigned char *temp;
-		int state=0;
-		while(state!=5)
-		{
-			switch(state)
-			{
-				case 0:
-				read(fd,temp, 1);
-				if(*temp==SET[0])
-					state=1;
-				break;
-				case 1:
-				read(fd,temp, 1);
-				if(*temp==SET[1])
-					state=2;
-				else if(*temp!=SET[0])
-					state=0;
-				break;	
-				case 2:
-				read(fd,temp, 1);
-				if(*temp==SET[0])
-					state=1;
-				else if(*temp==SET[2])
-					state=3;
-				else state=0;
-				break;	
-				case 3:
-				read(fd,temp, 1);
-				if(*temp==SET[0])
-					state=1;
-				else if(*temp==SET[3])
-					state=4;
-				else state=0;
-				break;	
-				case 4:
-				
-				read(fd,temp, 1);
-				if(*temp==SET[4])
-					state=5;
-				else state=0;
-				break;	
-			}
-		}
-
-
-		unsigned char *aux = UA;
-		
-		int size = sizeof(UA) / sizeof(unsigned char);
-		int num = 0;
-		int missing = size;
-
-		while(missing > 0) {
-			num = write(fd,aux,missing);
-			aux += num;
-			missing -= num;
-		}
-	}
-}
 
 int llwrite(int fd, char * buffer, int length);
 /*
